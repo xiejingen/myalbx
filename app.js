54bd829b23af29710610d457a44f22f11b7041aa
xiejingen <15277472829>
2019-05-16 13:29:09 +0800

    实现左边评论的小功能

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express')
 //引入路由模块
 var router = require('./router.js')
 //引入ejs
-var ejs=require('ejs')
+var ejs = require('ejs')
+//引入bodyParse
+var bodyParser=require('body-parser')
 //创建app应用程序服务器
 var app = express()
 //监听端口
@@ -16,7 +18,8 @@ app.use('/uploads',express.static('uploads'))
 app.set('view engine', 'ejs')
 //配置ejs的模板文件夹,ejs会自动的在指定的目录下寻找文件
 app.set('views',__dirname+'/views')
-
+//添加bodyParser处理
+app.use(bodyParser.urlencoded({extended:false}))
 //使用中间件在当前应用上挂载路由
 //实现路由的配置
 app.use(router)
---
+++ assets/js/admin_aside.js
@@ -0,0 +1,28 @@
+$(function () { 
+  //在路由满足#menu-posts的时候添加样式和属性
+  //在满足#menu-settings的时候做出同样的处理
+  //判断是否有参数
+  var index = location.href.indexOf('?')
+  var routername
+  if (index == -1) {
+    routername = location.href.substring(location.href.lastIndexOf('/')+1)
+  } else { 
+    routername=location.href.substring(location.href.lastIndexOf('/')+1,index)
+  }
+  //获取当前元素对元素进行属性设置
+  var menu_posts = $('#menu-posts')
+  if (routername == 'posts' || routername == 'post-add' || routername == 'categories') {
+    menu_posts.addClass('in')
+    menu_posts.attr('aria-expanded',true)
+  }
+  
+  //菜单设置
+  var menu_settings = $('#menu-settings')
+  if (routername == 'nav-menus' || routername == 'slides' || routername == 'settings') { 
+    menu_settings.addClass('in')
+    menu_settings.attr('aria-expanded',true)
+  }
+  
+})
+  // 为当前li元素添加样式
+  $('#'+routername).addClass('active')
